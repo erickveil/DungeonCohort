17f31ffa611d7a56c3b7bc3c2921182678d69324
eveil <erick@erickveil.net>
2019-11-04 10:08:08 -0800

    Basic random monster selection
    Also added output methods for easy styling

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.rtb_rndMonstOut = new System.Windows.Forms.RichTextBox();
+            this.bu_monster = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtb_rndMonstOut
+            // 
+            this.rtb_rndMonstOut.Location = new System.Drawing.Point(467, 12);
+            this.rtb_rndMonstOut.Name = "rtb_rndMonstOut";
+            this.rtb_rndMonstOut.Size = new System.Drawing.Size(321, 426);
+            this.rtb_rndMonstOut.TabIndex = 0;
+            this.rtb_rndMonstOut.Text = "";
+            // 
+            // bu_monster
+            // 
+            this.bu_monster.Location = new System.Drawing.Point(12, 12);
+            this.bu_monster.Name = "bu_monster";
+            this.bu_monster.Size = new System.Drawing.Size(138, 23);
+            this.bu_monster.TabIndex = 1;
+            this.bu_monster.Text = "Random Monster";
+            this.bu_monster.UseVisualStyleBackColor = true;
+            this.bu_monster.Click += new System.EventHandler(this.bu_monster_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bu_monster);
+            this.Controls.Add(this.rtb_rndMonstOut);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox rtb_rndMonstOut;
+        private System.Windows.Forms.Button bu_monster;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,85 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Darkmoor;
 
 namespace DungeonCohort
 {
     public partial class Form1 : Form
     {
+        AncestryIndex _ancestryIndex;
+
+        
         public Form1()
         {
             InitializeComponent();
+
+            _ancestryIndex = AncestryIndex.Instance;
+            _ancestryIndex.LoadAllSources();
+        }
+
+        public void Print(RichTextBox target, string message, Font font)
+        {
+            int msgStart = target.Text.Length;
+            int msgLen = message.Length;
+            target.AppendText(message);
+            target.Select(msgStart, msgLen);
+            target.SelectionFont = font;
+        }
+
+        public void PrintH1(RichTextBox target, string msg)
+        {
+            var typeface = "ITC Avant Garde Gothic LT";
+            var size = 11;
+            var style = FontStyle.Bold;
+            var font = new Font(typeface, size, style);
+            Print(target, msg, font);
+        }
+
+        public void PrintH2(RichTextBox target, string msg)
+        {
+            var typeface = "ITC Avant Garde Gothic LT";
+            var size = 9;
+            var style = FontStyle.Bold;
+            var font = new Font(typeface, size, style);
+            msg = msg.ToUpper();
+            Print(target, msg, font);
+        }
+
+        public void PrintH3(RichTextBox target, string msg)
+        {
+           var typeface = "ITC Avant Garde Gothic LT";
+           var size = 9;
+           var style = FontStyle.Bold;
+           var font = new Font(typeface, size, style);
+           Print(target, msg, font);
+        }
+
+        public void PrintBody(RichTextBox target, string msg)
+        {
+           var typeface = "AvantGarde Normal";
+           var size = 11;
+           var style = FontStyle.Regular;
+           var font = new Font(typeface, size, style);
+           Print(target, msg, font);
+        }
+
+        public void PrintBodyBold(RichTextBox target, string msg)
+        {
+           var typeface = "AvantGarde Normal";
+           var size = 11;
+           var style = FontStyle.Bold;
+           var font = new Font(typeface, size, style);
+           Print(target, msg, font);
+        }
+
+        private void bu_monster_Click(object sender, EventArgs e)
+        {
+            RichTextBox target = rtb_rndMonstOut;
+            target.Clear();
+            Ancestry monster = _ancestryIndex.GetRandomAncestry();
+            string monsterName = monster.Name;
+            PrintBodyBold(target, monsterName);
         }
     }
 }
