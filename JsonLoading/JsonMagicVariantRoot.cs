80c16dd2cd57fdde2296cb14a846ae29a581c33b
eveil <erick@erickveil.net>
2020-01-04 13:20:44 -0800

    Included variants that were skipped because of source indirection
    The json source for many items (sword +1, etc) redirects part of its
    stats because the weapon +1 could be any weapon, and the source used the
    data to build these items in detail. I don't, so I just create a generic
    item.
    
    It means I miss some of the item filtering, because for some reason the
    rarity and frequency weren't tied to the base magic item, but whatever.

--- JsonLoading/JsonMagicVariantRoot.cs
+++ JsonLoading/JsonMagicVariantRoot.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace DungeonCohort
 {
@@ -17,8 +19,36 @@ namespace DungeonCohort
                 if (variantData.name != name) { continue; }
                 return variantData;
             }
-            Console.WriteLine("Could not find magicVariant by name: " + name);
-            return null;
+            //Console.WriteLine("Could not find magicVariant by name: " + name);
+            // We're going to make one up so it's not excluded and I don't
+            // have to navigate json that wasn't built with my use in mind.
+
+            // remove some formatting
+            name = _removeSubstring("{@item ", name);
+            name = _removeSubstring("}", name);
+
+            var newVariant = new JsonMagicVariants();
+            newVariant.name = name;
+            newVariant.type = "";
+            newVariant.requires = new JArray();
+
+            var inherits = new JsonMagicVariantsInherits();
+            inherits.entries = new JArray();
+            inherits.lootTables = new JArray();
+
+            newVariant.inherits = inherits;
+            
+            return newVariant;
         }
+
+        private string _removeSubstring(string needle, string haystack)
+        {
+            int index = haystack.IndexOf(needle);
+            string cleanString = (index < 0)
+                ? haystack
+                : haystack.Remove(index, needle.Length);
+            return cleanString;
+        }
+
     }
 }
